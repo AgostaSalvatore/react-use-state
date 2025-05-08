583ec3dab878d246e554046a1fd318d45caa1aea
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-08 13:31:57 +0200

    [UPD] create html skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import languages from './data/languages'
 
 const App = () => {
+  console.log(languages)
   return (
+    <>
     <div>
-      <h1></h1>
+      <h1>Learn Web Development</h1>
     </div>
+    <div className='container'>
+      <div className="accordion">
+        <div className="accordion-item">
+          <ul>
+        {languages.map(language => (
+          <li key={language.id}>
+            <button className='btn'>
+                {language.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+        </div>
+      </div>
+    </div>
+    </>
+    
   )
 }
 
