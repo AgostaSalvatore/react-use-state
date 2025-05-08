84983ff609ff62471b749a6117b09366dc3abb01
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-08 16:04:08 +0200

    [UPD] added logic to color a single button while showed content

--- src/components/accordion/AccordionTop.jsx
+++ src/components/accordion/AccordionTop.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import languages from '../../data/languages'
 
 const AccordionTop = ({ setActiveLanguage }) => {
+    const [activeId, setActiveId] = useState(null)
     return (
         <div>
             <div className="accordion-buttons">
                 <ul>
                     {languages.map(language => (
                         <li key={language.id}>
-                            <button className='btn' onClick={() => setActiveLanguage(language)}>
+                            <button className={`btn ${activeId === language.id ? 'active' : ''}`} 
+                                onClick={() => {
+                                    setActiveLanguage(language);
+                                    setActiveId(language.id);
+                                }}>
                                 {language.title}
                             </button>
                         </li>
